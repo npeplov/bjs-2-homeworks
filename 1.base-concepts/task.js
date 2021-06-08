4b9502876991ada1d35d8480f0b7d45f85da56b4
Nikita Peplov <npeplov@yandex.ru>
2021-06-08 11:05:39 +0300

    juliana

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,21 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+ if (a==0) return ["a= 0, это недопустимое значение", "нет значения", "нет значения"];
+  const discriminant = b ** 2 - 4 * a * c;
+  if (discriminant > 0) {
+    let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return [discriminant,x1, x2];
+  } else if
+    (discriminant === 0) {
+    let x1 = (-b/(2*a));
+   
+    return [x1];
+  } else if
+    (discriminant < 0) {
+   
+    return  [];
+  }
+};
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
